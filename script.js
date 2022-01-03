7317428e2fc3ababe73b9055b544e5b04d036f95
Breno L. Lopes <brernolopes@hotmail.com>
2022-01-03 18:04:12 -0300

    Desafio 2 ok

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let newImage = document.querySelector("#meme-insert")
+let imageContainer = document.querySelector("#meme-image")
 
 function memeText(){
-  
-  
   const text = document.querySelector("#text-input").value
   const textMeme = document.querySelector("#meme-text")
-  textMeme.innerText = text
-  console.log(text)
+  textMeme.firstElementChild.innerText = text
+}
+
+
+function updateImage(){
 }
