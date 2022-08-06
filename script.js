57e2e9942cd81f6284e88012ee292aa4c983166c
Breno L. Lopes <brernolopes@hotmail.com>
2022-08-06 17:17:05 -0300

    fix: funções passadas pelo addEventListner para não mais serem chamadas no html

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
+const textInput = document.getElementById('text-input');
 
-function memeText(){
-  const text = document.querySelector("#text-input").value
-  const textMeme = document.querySelector("#meme-text")
-  textMeme.firstElementChild.innerText = text
-}
+const memeText = () => {
+  const text = document.querySelector('#text-input').value;
+  const textMeme = document.querySelector('#meme-text');
+  textMeme.firstElementChild.innerText = text;
+};
 
+textInput.addEventListener('keydown', memeText);
 
-function changeBoard(element){
-  let borda = document.querySelector("#meme-image-container")
-  if(element.id === "fire"){
-    borda.style.border = "3px"
-    borda.style.borderColor = "red"
-    borda.style.borderStyle = "dashed"
-  }else if(element.id === "water"){
-    borda.style.border = "5px"
-    borda.style.borderColor = "blue"
-    borda.style.borderStyle = "double"
-  }else if(element.id === "earth"){
-    borda.style.border = "6px"
-    borda.style.borderColor = "green"
-    borda.style.borderStyle = "groove"
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+
+const changeBoard = ({ target }) => {
+  const borda = document.querySelector('#meme-image-container');
+  if (target.id === 'fire') {
+    borda.style.border = '3px';
+    borda.style.borderColor = 'red';
+    borda.style.borderStyle = 'dashed';
+  } else if (target.id === 'water') {
+    borda.style.border = '5px';
+    borda.style.borderColor = 'blue';
+    borda.style.borderStyle = 'double';
+  } else if (target.id === 'earth') {
+    borda.style.border = '6px';
+    borda.style.borderColor = 'green';
+    borda.style.borderStyle = 'groove';
   }
-}
+};
 
-let changeImage = function (event){
-  let newMeme = document.querySelector("#meme-image")
-  newMeme.src = URL.createObjectURL(event.target.files[0])
-  newMeme.onload = function(){
-    URL.revokeObjectURL(newMeme.src)
-  }
-}
+fire.addEventListener('click', changeBoard);
+water.addEventListener('click', changeBoard);
+earth.addEventListener('click', changeBoard);
 
-let fixedMeme = function (teste){
-  let newMeme = document.querySelector("#meme-image")
-  newMeme.src = teste.src
-}
+const upMeme = document.getElementById('meme-insert');
+
+const changeImage = ({ target }) => {
+  const newMeme = document.querySelector('#meme-image');
+  newMeme.src = URL.createObjectURL(target.files[0]);
+  newMeme.onload = () => {
+    URL.revokeObjectURL(newMeme.src);
+  };
+};
+
+upMeme.addEventListener('change', changeImage);
+
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+
+const fixedMeme = ({ target }) => {
+  const newMeme = document.querySelector('#meme-image');
+  newMeme.src = target.src;
+};
+
+meme1.addEventListener('click', fixedMeme);
+meme2.addEventListener('click', fixedMeme);
+meme3.addEventListener('click', fixedMeme);
